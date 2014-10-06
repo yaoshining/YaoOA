017200ac04db4d426357d902c96e2ec50c9b2a6f
yaoshining <ysnlvlqw@msn.com>
2014-10-06 22:44:42 +0800

    Add basic authorization to restful webservices controller.

--- OA/src/main/java/com/yao/oa/controllers/user/AuthenticationController.java
+++ OA/src/main/java/com/yao/oa/controllers/user/AuthenticationController.java
@@ -2,6 +2,8 @@ package com.yao.oa.controllers.user;
 
 import com.yao.oa.services.user.UsersService;
 import net.sf.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -15,22 +17,25 @@ import javax.annotation.Resource;
  * Created by 世宁 on 2014/10/6 0006.
  */
 @RestController
+@RequestMapping("/services/auth")
 public class AuthenticationController {
     @Resource
     private UsersService usersService;
-    @RequestMapping("/login")
-    public String login(String username,String password) {
+    @RequestMapping(value = "/login",produces = {"application/json"})
+    public ResponseEntity<String> login(String username,String password) {
         Authentication request = new UsernamePasswordAuthenticationToken(username,password);
-        boolean isAuth = false;
-        try {
-            Authentication authentication = usersService.authenticate(request);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            isAuth = true;
-        } catch (AuthenticationException e){
-            SecurityContextHolder.setContext(SecurityContextHolder.createEmptyContext());
-        }
+        HttpStatus status = HttpStatus.OK;
         JSONObject jsonObj = new JSONObject();
-        jsonObj.put("status",isAuth);
-        return jsonObj.toString();
+        try {
+//            Authentication authentication = usersService.authenticate(request);
+//            SecurityContextHolder.getContext().setAuthentication(authentication);
+            status = HttpStatus.OK;
+//            jsonObj.put("status",true);
+        } catch (AuthenticationException e){
+//            SecurityContextHolder.setContext(SecurityContextHolder.createEmptyContext());
+            jsonObj.put("status",false);
+            jsonObj.put("message","Bad Credentials");
+        }
+        return new ResponseEntity<String>(jsonObj.toString(), status);
     }
 }
